e2fd8cb024dadef00473d34340eccad97297f347
Guillen40 <guillenkevin442@gmail.com>
2026-02-02 20:49:19 -0400

    Add files via upload

---
+++ Programacion/Tarea_Escrita/Tarea_2/Programa 2.4.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main()
+{
+    int edad, pase;
+
+    printf("Control de acceso \n");
+    printf("que edad tienes?: ");
+    scanf("%d", &edad);
+
+    if (edad >= 18) {
+
+        printf("tienes pase VIP? (1 para SI, 0 para NO): ");
+        scanf("%d", &pase);
+
+        if (pase == 1)
+            {
+            printf("Bienvenido! Pasa a la zona lounge con barra libre.\n");
+        }
+        else
+            {
+            printf("Pasa a la zona general. ¡Diviertete!\n");
+        }
+
+    }
+    else
+        {
+
+        printf("Lo siento, acceso denegado.\n");
+    }
+
+    return 0;
+}
