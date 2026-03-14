41881292986ac2e4b5fbd9a74b7c527e8fe22be9
Guillen40 <guillenkevin442@gmail.com>
2026-03-13 22:39:10 -0300

    Add files via upload

---
+++ Programacion/Tarea_Escrita/Tarea_2/Programa 2.12.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <math.h>
+
+void main(void)
+{
+    float RES;
+    int T, P, N;
+    printf("Ingrese los valores de T, P y N: ");
+    scanf("%d %d %d", &T, &P, &N);
+    if (P != 0)
+    {
+        RES = pow(T / P, N);
+        if (RES == pow(T, N) / pow(P, N))
+            printf("\nSe comprueba la igualdad");
+        else
+            printf("\nNo se comprueba la igualdad");
+    }
+    else
+        printf("\nP tiene que ser diferente de cero");
+}
