859e5cd286cbdc985e4d3264f25bb6f3ee479d3e
Guillen40 <guillenkevin442@gmail.com>
2026-03-13 23:34:14 -0300

    Update Programa 3.6.c

--- Programacion/Tarea_Escrita/Tarea_3/Programa 3.6.c
+++ Programacion/Tarea_Escrita/Tarea_3/Programa 3.6.c
@@ -1,11 +1,4 @@
 #include <stdio.h>
-
-/* Nomina de profesores.
-El programa, al recibir como datos los salarios de los profesores de una
-universidad, obtiene la nomina y el promedio de los salarios.
-I: variable de tipo entero.
-SAL, NOM y PRO: variables de tipo real. */
-
 void main(void)
 {
     int I = 0;
@@ -18,8 +11,8 @@ void main(void)
         I = I + 1;
         printf("Ingrese el salario del profesor -0 para terminar: \t");
         scanf("%f", &SAL);
-    }
-    while (SAL);
+        }
+while (SAL);
     PRO = NOM / I;
     printf("\nNomina: %.2f \t Promedio de salarios: %.2f", NOM, PRO);
-}
+    }
