bf40ebf29e7ac3eb11f3ec755f011941094d1e58
Guillen40 <guillenkevin442@gmail.com>
2026-04-24 02:42:30 -0300

    Add files via upload

---
+++ Tarea_2/Programa 2.6.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void main(void)
+{
+    float SAL;
+    int NIV;
+    printf("Ingrese el nivel academico del profesor:");
+    scanf("%d", &NIV);
+    printf("Ingrese el salario: ");
+    scanf("%f", &SAL);
+    switch(NIV){
+        case 1: SAL = SAL * 1.035; break;
+        case 2: SAL = SAL * 1.041; break;
+        case 3: SAL = SAL * 1.048; break;
+        case 4: SAL = SAL * 1.053; break;
+        }
+    printf("\nNuevo salario: %8.2f", SAL);
+}
